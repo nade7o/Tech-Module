5d6c6538d0211781b6cbd1cf538da1cdde53a601
Nadya Karaivanova <karaivanova.nadya@gmail.com>
2019-03-21 12:21:12 +0200

    Add my  exercises from Mid exam March 2019

---
+++ TM_MidExam_March2019/02.SeizeOfFIre/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SeizeFire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("#").ToArray();
+            int water = int.Parse(Console.ReadLine());
+            double totalEffort = 0;
+            double totalFire = 0;
+            List<int> values = new List<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] currentFireCell = input[i].Split(" = ").ToArray();
+                string typeOfFire = currentFireCell[0];
+                int value = int.Parse(currentFireCell[1]);
+                double currentEffort;
+                if (water <= 0)
+                {
+                    return;
+                }
+                if (typeOfFire == "High")
+                {
+                    if (value >= 81 && value <= 125)
+                    {
+                        water -= value;
+                        if (water < 0)
+                        {
+                            break;
+                        }
+                        currentEffort = 0.25 * value;
+                        totalEffort += currentEffort;
+                        totalFire += value;
+                        values.Add(value);
+                    }
+                }
+                else if (typeOfFire == "Medium")
+                {
+                    if (value >= 51 && value <= 80)
+                    {
+                        water -= value;
+                        if (water < 0)
+                        {
+                            break;
+                        }
+                        currentEffort = 0.25 * value;
+                        totalEffort += currentEffort;
+                        values.Add(value);
+                        totalFire += value;
+                    }
+                }
+                else if (typeOfFire == "Low")
+                {
+                    if (value >= 1 && value <= 50)
+                    {
+                        water -= value;
+                        if (water < 0)
+                        {
+                            break;
+                        }
+                        currentEffort = 0.25 * value;
+                        totalEffort += currentEffort;
+                        values.Add(value);
+                        totalFire += value;
+                    }
+                }
+            }
+            Console.WriteLine("Cells:");
+            foreach (var item in values)
+            {
+                Console.WriteLine($" - {item}");
+            }
+            Console.WriteLine($"Effort: {totalEffort:f2}");
+            Console.WriteLine($"Total Fire: {totalFire}");
+        }
+    }
+}
---
+++ TM_MidExam_March2019/03.TheFinalQuest/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _03.TheFinalQuest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split(" ").ToList();
+            string input = string.Empty;
+
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "Stop")
+                {
+                    Console.WriteLine(string.Join(" ", words));
+                    break;
+                }
+                string[] tokens = input.Split(" ");
+                string command = tokens[0];
+
+                if (command == "Delete")
+                {
+                    int index = int.Parse(tokens[1]);
+                    if (index >= 0 && index < words.Count)
+                    {
+                        if (index + 1 > 0 && index + 1 < words.Count)
+                        {
+                            words.RemoveAt(index + 1);
+                        }
+                    }
+
+                }
+                else if (command == "Swap")
+                {
+                    string word1 = tokens[1];
+                    string word2 = tokens[2];
+
+                    if (words.Contains(word1) && words.Contains(word2))
+                    {
+                        int index1 = words.IndexOf(word1);
+                        int index2 = words.IndexOf(word2);
+
+                        if (index1 >= 0 && index1 < words.Count && index2 >= 0 && index2 < words.Count)
+                        {
+                            words.Insert(index1, word2);
+                            words.RemoveAt(index1 + 1);
+                            words.Insert(index2, word1);
+                            words.RemoveAt(index2 + 1);
+                        }
+
+                    }
+                }
+                else if (command == "Put")
+                {
+                    string word = tokens[1];
+                    int index = int.Parse(tokens[2]);
+                    if (index >= 0)
+                    {
+                        int previousIndex = index - 1;
+                        if (previousIndex >= 0 && previousIndex <= words.Count)
+                        {
+                            words.Insert(previousIndex, word);
+                        }
+                    }
+
+                }
+                else if (command == "Sort")
+                {
+                    words.Sort();
+                    words.Reverse();
+                }
+                else if (command == "Replace")
+                {
+                    string word1 = tokens[1];
+                    string word2 = tokens[2];
+                    if (words.Contains(word2))
+                    {
+                        int index = words.IndexOf(word2);
+                        words.Insert(index, word1);
+                        if (index + 1 < words.Count)
+                        {
+                            words.RemoveAt(index + 1);
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
