9f7c28bc8d6310cb9be27302b5f49f0f65e3f732
Nadya Karaivanova <karaivanova.nadya@gmail.com>
2019-03-29 15:23:10 +0200

    Phonebook-web app

---
+++ TM_9_WebApp/Phonebook/Data/DataAccess.cs
@@ -0,0 +1,14 @@
+﻿using Phonebook.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phonebook.Data
+{
+    public class DataAccess
+    {
+        //Write List which will keep all the data about contacts
+        public static List<Contact> Contacts { get; set; } = new List<Contact>();
+    }
+}
