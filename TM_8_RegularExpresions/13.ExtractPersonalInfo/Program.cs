a2f1dd96c6e525ac10cb7c7a45784a44532dcd8f
nade7o <karaivanova.nadya@gmail.com>
2019-03-31 21:16:55 +0300

    Add solutions to TM_8_RegularExpresions

---
+++ TM_8_RegularExpresions/13.ExtractPersonalInfo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01.ExtractPersonalInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string name = string.Empty;
+            string age = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                // Here is a name @George| and an age #18*
+                string line = Console.ReadLine();
+                int startIndexName = line.IndexOf('@');
+                int endIndexName = line.IndexOf('|');
+                name = line.Substring(startIndexName + 1, endIndexName - startIndexName - 1);
+                int startIndexAge = line.IndexOf('#');
+                int endIndexAge = line.IndexOf('*');
+                age = line.Substring(startIndexAge + 1, endIndexAge - startIndexAge - 1);
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
