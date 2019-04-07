3f67f0ca51a430a8f8f25ff34cc79b84f2c28c72
Nadya Karaivanova <karaivanova.nadya@gmail.com>
2019-04-07 18:22:32 +0300

    Add my web project from Demo Final Exam

---
+++ TM_DemoFinalExam_WebProject_Skeleton-C#/Library/Controllers/LibraryController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.Data;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var db = new LibraryDbContext())
+            {
+                var allBooks = db.Books.ToList();
+                return View(allBooks);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(string title, string author, double price)
+        {
+                if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(author) || double.IsNegativeInfinity(price))
+                {
+                    return RedirectToAction("Index");
+                }
+                Book book = new Book
+                {
+                    Title = title,
+                    Author = author,
+                    Price = price
+                };
+                using (var db = new LibraryDbContext())
+                {
+                    db.Books.Add(book);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db = new LibraryDbContext())
+            {
+                var bookToEdit = db.Books.Find(id);
+                return View(bookToEdit);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            using (var db = new LibraryDbContext())
+            {
+                db.Books.Update(book);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (var db = new LibraryDbContext())
+            {
+                var bookToDelete = db.Books.Find(id);
+                return View(bookToDelete);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Book book)
+        {
+            using (var db = new LibraryDbContext())
+            {
+                db.Books.Remove(book);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TM_DemoFinalExam_WebProject_Skeleton-C#/Library/Data/LibraryDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Library.Models;
+
+namespace Library.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        private const string ConnectionString = @"Server=LAPTOP-DAP35E42\SQLEXPRESS;Database=LibraryDbContext;Integrated Security=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
