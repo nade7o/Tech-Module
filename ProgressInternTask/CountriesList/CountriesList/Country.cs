c0f9cf2e87bd4a0e6a2ddfcfe3469e6597398b12
nade7o <karaivanova.nadya@gmail.com>
2019-04-11 08:22:39 +0300

    Progress Intern Task
    Country List - .NET application using WPF framework

---
+++ ProgressInternTask/CountriesList/CountriesList/Country.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountriesList
+{
+    class Country
+    {
+        public string CountryName { get; set; }
+        public string Capital { get; set; }
+        public string Flag { get; set; }
+
+        public Country(string CountryName, string Capital )
+        {
+            this.CountryName = CountryName;
+            this.Capital = Capital;
+        }
+    }
+}
