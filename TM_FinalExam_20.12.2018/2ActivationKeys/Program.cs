f1de6549e63757685d14ead1bf19f4648fe184d7
Nadya Karaivanova <karaivanova.nadya@gmail.com>
2019-04-13 20:06:10 +0300

    TM_FinalExam_20.12.2018(Preparation)

---
+++ TM_FinalExam_20.12.2018/2ActivationKeys/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2ActivationKeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split("&");
+
+            List<string> keys = new List<string>();
+
+            
+            foreach (var key in line)
+            {
+                if (key.Length == 16 || key.Length == 25)
+                {
+                    bool isValid = true;
+
+                    foreach (var symbol in key)
+                    {
+                        if (!char.IsLetterOrDigit(symbol))
+                        {
+                            isValid = false;
+                        }
+                    }
+                    if (isValid)
+                    {
+                        keys.Add(key);
+                    }
+                }
+            }
+            for (int i = 0; i < keys.Count; i++)
+            {
+                if (keys[i].Length == 16)
+                {
+                    keys[i] = keys[i].Insert(4, "-");
+                    keys[i] = keys[i].Insert(9, "-");
+                    keys[i] = keys[i].Insert(14, "-");
+                }
+                else if (keys[i].Length == 25)
+                {
+                    keys[i] = keys[i].Insert(5, "-");
+                    keys[i] = keys[i].Insert(11, "-");
+                    keys[i] = keys[i].Insert(17, "-");
+                    keys[i] = keys[i].Insert(23, "-");
+                }
+            }
+            for (int i = 0; i < keys.Count; i++)
+            {
+                for (int j = 0; j < keys[i].Length; j++)
+                {
+                    char currentChar = keys[i][j];
+                    if (char.IsDigit(currentChar))
+                    {
+                        int currentDigit = 9 - int.Parse(currentChar.ToString());
+                        keys[i] = keys[i].Remove(j,1);
+                        keys[i] = keys[i].Insert(j, currentDigit.ToString());
+                    }
+                }
+                keys[i] = keys[i].ToUpper();
+            }
+            Console.WriteLine(string.Join(", ", keys));
+
+        }
+    }
+}
---
+++ TM_FinalExam_20.12.2018/GameStore/Controllers/GameController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GameStore.Data;
+using GameStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var db = new GameStoreDbContext())
+            {
+                var allGames = db.Games.ToList();
+                return View(allGames);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(int id, string name, string dlc, double price, string platform)
+        {
+            Game game = new Game
+            {
+                Id = id,
+                Name = name,
+                Dlc = dlc,
+                Price = price,
+                Platform = platform
+        };
+            using (var db = new GameStoreDbContext())
+            {
+                db.Add(game);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db = new GameStoreDbContext())
+            {
+                var gameToEdit = db.Games.Find(id);
+                return View(gameToEdit);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Game game)
+        {
+            using (var db = new GameStoreDbContext())
+            {
+                db.Games.Update(game);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (var db = new GameStoreDbContext())
+            {
+                var gameToDelete = db.Games.Find(id);
+                return View(gameToDelete);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Game game)
+        {
+            using (var db = new GameStoreDbContext())
+            {
+                db.Games.Remove(game);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
