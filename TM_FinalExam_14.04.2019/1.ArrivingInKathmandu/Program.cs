80627d1b5b2ead9b613892d946a711d1bf2d4011
Nadya Karaivanova <karaivanova.nadya@gmail.com>
2019-04-16 13:38:07 +0300

    Add solutions from Final exam on 14.04.2019

---
+++ TM_FinalExam_14.04.2019/1.ArrivingInKathmandu/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1.ArrivingInKathmandu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = string.Empty;
+
+            string patern = @"^(?<name>[A-Za-z0-9!@#$?]+)=(?<length>\d+)<<(?<code>.+)$";
+
+            while ((line = Console.ReadLine()) != "Last note")
+            {
+                if (Regex.Match(line, patern).Success)
+                {
+                    foreach (Match item in Regex.Matches(line, patern))
+                    {
+                        string name = string.Empty;
+                        int length = int.Parse(item.Groups["length"].Value);
+                        string code = item.Groups["code"].Value;
+                        if (length == code.Length)
+                        {
+                            foreach (var i in item.Groups["name"].Value)
+                            {
+                                if (char.IsLetterOrDigit(i))
+                                {
+                                    name += i;
+                                }
+                            }
+                            Console.WriteLine($"Coordinates found! {name} -> {code}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nothing found!");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+            }
+        }
+    }
+}
---
+++ TM_FinalExam_14.04.2019/2.OnTheWayToAnnapurna/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.OnTheWayToAnnapurna
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Dictionary<string, List<string>> diary = new Dictionary<string, List<string>>();
+
+            while ((input=Console.ReadLine()) != "END")
+            {
+                string[] tokens = input.Split("->", StringSplitOptions.RemoveEmptyEntries);
+                
+                if (tokens.Length == 3)
+                {
+                    string action = tokens[0];
+                    string store = tokens[1];
+                    string[] items = tokens[2].Split(",");
+
+                    if (items.Length==1)
+                    {
+                        string item = items[0];
+                        if (!diary.ContainsKey(store))
+                        {
+                            diary.Add(store, new List<string>());
+                            diary[store].Add(item);
+                        }
+                        else
+                        {
+                            diary[store].Add(item);
+                        }
+                    }
+                    if (items.Length > 1)
+                    {
+                        if (!diary.ContainsKey(store))
+                        {
+                            diary.Add(store, new List<string>());
+                            for (int i = 0; i < items.Length; i++)
+                            {
+                                diary[store].Add(items[i]);
+                            }
+                        }
+                        else
+                        {
+                            for (int i = 0; i < items.Length; i++)
+                            {
+                                diary[store].Add(tokens[i]);
+                            }
+                        }
+                    }
+
+                }
+
+                else if (tokens.Length == 2)
+                {
+                    string action = tokens[0];
+                    string store = tokens[1];
+
+                    if (diary.ContainsKey(store))
+                    {
+                        diary.Remove(store);
+                    }
+                }
+            }
+            Console.WriteLine("Stores list:");
+            foreach (var item in diary.OrderByDescending(x=>x.Value.Count).ThenByDescending(x=>x.Key))
+            {
+                Console.WriteLine($"{item.Key}");
+                foreach (var i in item.Value)
+                {
+                    Console.WriteLine($"<<{i}>>");
+                }
+            }
+
+        }
+    }
+}
